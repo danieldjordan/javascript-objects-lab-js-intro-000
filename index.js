cb2741680cfef5069a7d642394e401f2a0adee42
danieldjordan@gmail.com <danieldjordan@gmail.com>
2018-08-24 23:07:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function updateObjectWithKeyAndValue(object, key, value){
   return newObj;
 }
 
-destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value
   return object
 }
